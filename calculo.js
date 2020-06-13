1cb584b6fdf50c51e650bc2162ef476cfe3bfd1f
Allan <32887387+AllanCrazyProgrammer@users.noreply.github.com>
2020-06-13 17:13:07 -0500

    added 2 and 10

--- calculo.js
+++ calculo.js
@@ -9,7 +9,9 @@ module.exports = {
     var din100 = 0;
     var din50 = 0;
     var din20 = 0;
+    var din10 = 0;
     var din5 = 0;
+    var din2 = 0;
     var din1 = 0;
     var monedas = 0;
 
@@ -47,46 +49,65 @@ module.exports = {
         din20 += 1;
       }
 
+      while (data[i] >= 10 && data[i] > 0) {
+        data[i] -= 10;
+        din10 += 1;
+      }
+
       while (data[i] < 20 && data[i] >= 5) {
         data[i] -= 5;
         din5 += 1;
+      }
+
+      if (data[i] === 4 && data[i] > 0) {
+        data[i] -= 4;
+        din2 += 2;
+      }
+
+      if (data[i] === 2 && data[i] > 0) {
+        data[i] -= 2;
+        din2 += 1;
+      }
+
+      if (data[i] === 4 && data[i] > 0) {
+        data[i] -= 4;
+        din2 += 2;
       }
 
       if (data[i] < 5 && data[i] > 0) {
         din1 += data[i];
       }
 
-      monedas = din5*5 + din1
+      monedas = din5 * 5 + din1
     }
 
     console.log(
       `500 = ` +
-        din500 +
-        `\n` +
-        `200 = ` +
-        din200 +
-        `\n` +
-        `100 = ` +
-        din100 +
-        `\n` +
-        `50 = ` +
-        din50 +
-        `\n` +
-        `20 = ` +
-        din20 +
-        `\n` +
-        `5 = ` +
-        din5 +
-        `\n` +
-        `o ` +
-        din5 / 2 +
-        ` de 10` +
-        `\n` +
-        `1 = ` +
-        din1 +
-        `\n` +
-        `Monedas = ` +
-        monedas
+      din500 +
+      `\n` +
+      `200 = ` +
+      din200 +
+      `\n` +
+      `100 = ` +
+      din100 +
+      `\n` +
+      `50 = ` +
+      din50 +
+      `\n` +
+      `20 = ` +
+      din20 +
+      `\n` +
+      `10 = ` +
+      din10 +
+      `\n` +
+      `5 = ` +
+      din5 +
+      `\n` +
+      `1 = ` +
+      din1 +
+      `\n` +
+      `2 = ` +
+      din2
     );
   },
 };
